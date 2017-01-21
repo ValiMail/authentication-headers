14eeea8f1d61385ebe1420d6b0bf423ba727cbc7
gene <gene@valimail.com>
2017-01-20 16:39:31 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "authentication-headers",
+    version = "0.5",
+    author = "Gene Shuman",
+    author_email = "gene@valimail.com",
+    description = ("A library wrapping email authentication header verification and generation."),
+    license = "MIT",
+    keywords = "email headers SPF DKIM DMARC ARC",
+    url = "",
+    packages=['authentication-headers', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
