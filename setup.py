1b52264b59fc8b5fd180c0e931749157d8fae537
Scott Kitterman <scott@kitterman.com>
2018-11-17 17:15:42 -0500

    Switch to use upstream dkimpy, bump version to 0.9.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "authheaders",
-    version = "0.9.2",
+    version = "0.9.3",
     author = "Gene Shuman",
     author_email = "gene@valimail.com",
     description = ("A library wrapping email authentication header verification and generation."),
@@ -18,10 +18,8 @@ setup(
     package_data={
         'authheaders': ['public_suffix_list.txt'],
     },
-    dependency_links = ['http://github.com/ValiMail/dkimpy/tarball/master#egg=dkimpy-0.7.1'
-    ],
     install_requires = [
-        "valimail_dkimpy>=0.7.1",
+        "dkimpy>=0.7.1",
         "authres>=1.0.1",
         "publicsuffix",
         "ipaddress",
