679207ec4772e5e834aed1926b6ce3427344600d
Scott Kitterman <scott@kitterman.com>
2018-11-17 16:30:49 -0500

    0.9.2 from pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "authheaders",
-    version = "0.9.1",
+    version = "0.9.2",
     author = "Gene Shuman",
     author_email = "gene@valimail.com",
     description = ("A library wrapping email authentication header verification and generation."),
@@ -21,7 +21,7 @@ setup(
     dependency_links = ['http://github.com/ValiMail/dkimpy/tarball/master#egg=dkimpy-0.7.1'
     ],
     install_requires = [
-        "valimail_dkimpy>=0.7.2",
+        "valimail_dkimpy>=0.7.1",
         "authres>=1.0.1",
         "publicsuffix",
         "ipaddress",
