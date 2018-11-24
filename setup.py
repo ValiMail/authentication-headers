4eac615b05d3fd4f57b2bd2257c4e1c8547df250
Scott Kitterman <scott@kitterman.com>
2018-11-24 04:53:43 -0500

    - Added new pslupdate option to setup.py so embedded public suffix list copy     can be easily updated

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
 from setuptools import setup
+import distutils.cmd
+import distutils.log
+import setuptools
+import tempfile
+from urllib import request
+import os
+
+class UpdatePublicSuffixList(distutils.cmd.Command):
+    """Update embedded copy of PSL from publicsuffix.org."""
+
+    description = 'PSL update command - use prior to build/install commands'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Download to tempfile and move to authheaders/public_suffix_list.txt."""
+        tmpfile = tempfile.mkstemp(".tmp", dir="./authheaders")[1]
+        url = 'https://publicsuffix.org/list/effective_tld_names.dat'
+        self.announce(
+            'Updating PSL',
+            level=distutils.log.INFO)
+        request.urlretrieve(url, tmpfile)
+        os.rename(tmpfile, 'authheaders/public_suffix_list.txt')
 
 setup(
     name = "authheaders",
@@ -25,4 +53,7 @@ setup(
         "ipaddress",
         "dnspython"
     ],
+    cmdclass={
+        'pslupdate': UpdatePublicSuffixList,
+    },
 )
