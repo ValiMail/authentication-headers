1ce9b4b40cd3be2bd3f705c0298b9d9472dc90c0
Scott Kitterman <scott@kitterman.com>
2024-01-16 11:03:58 -0500

    Set version to 0.16.1 for release

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ DESC = """Python module for generating email authentication headers: Authheaders
 
 setup(
     name = "authheaders",
-    version = "0.16.0",
+    version = "0.16.1",
     author = "Gene Shuman",
     author_email = "gene@valimail.com",
     description = ("A library wrapping email authentication header verification and generation."),
